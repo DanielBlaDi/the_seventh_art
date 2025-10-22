11f7e430463ad10300474459b5c25eedfe7de368
julianAlbarra547 <jalbarracing@unal.edu.co>
2025-10-22 07:53:41 -0500

    Creación entidad historia.

--- Project/src/main/java/seventh_art/rocky/entity/Historia.java
+++ Project/src/main/java/seventh_art/rocky/entity/Historia.java
@@ -1,5 +1,40 @@
 package seventh_art.rocky.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(
+        name = "historia"
+)
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class Historia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Positive
+    @Column(nullable = false)
+    private String tiempo;              //En segundos
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "idRutina",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_historia_rutina")
+    )
+    private Rutina idRutina;
 }
