9edc3a73e933c4d4ca1292cacf5bb896580ef809
julianAlbarra547 <jalbarracing@unal.edu.co>
2025-11-24 14:55:32 -0500

    Creación del RutinaDTO y el RutinaRepositoru

---
+++ Project/src/main/java/seventh_art/rocky/repository/RutinaRepository.java
@@ -0,0 +1,6 @@
+package seventh_art.rocky.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import seventh_art.rocky.entity.Rutina;
+
+public interface RutinaRepository extends JpaRepository<Rutina, Long> {
+}
