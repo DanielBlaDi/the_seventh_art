df1f4db2b4e0f81a44763c1ad854240f4c257f7f
DanielBlaDi <dablancod@unal.edu.co>
2025-11-28 01:27:07 -0500

    Update tables 'Perfil' and 'MensajeMotivacional' from the data base

--- Project/src/main/java/seventh_art/rocky/entity/MensajeMotivacional.java
+++ Project/src/main/java/seventh_art/rocky/entity/MensajeMotivacional.java
@@ -2,13 +2,9 @@ package seventh_art.rocky.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -35,11 +31,4 @@ public class MensajeMotivacional {
     @Column(nullable = false, length = 255)
     private String descripcion;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(
-            name = "id_perfil",
-            nullable = false,
-            foreignKey = @ForeignKey(name = "fk_mensaje_motivacional_perfil")
-    )
-    private Perfil perfil;
 }
--- Project/src/main/java/seventh_art/rocky/entity/Perfil.java
+++ Project/src/main/java/seventh_art/rocky/entity/Perfil.java
@@ -15,6 +15,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -94,12 +95,24 @@ public class Perfil {
             foreignKey = @ForeignKey(name = "fk_perfil_usuario")
     )
     private Usuario usuario;
-
+    
     // ===== ManyToMany con Logro (lado inverso) =====
     @ManyToMany(mappedBy = "perfiles")
     @Builder.Default
     @JsonIgnore
     private Set<Logro> logros = new HashSet<>();
+
+
+    @ManyToMany
+    @JoinTable(
+        name = "perfil_mensaje_motivacional",
+        joinColumns = @JoinColumn(name = "id_perfil"),
+        inverseJoinColumns = @JoinColumn(name = "id_mensaje_motivacional")
+    )
+    @Builder.Default
+    private Set<MensajeMotivacional> mensajesMotivacionales = new HashSet<>();
+
+
 
 }
 
